bb2fcacff20ab0fbb3ff9e7f2cbf493364fe4d4d
Alexander Thiele <thiele@zeiss-izm.de>
2020-04-20 15:16:46 +0200

    Another try 2

--- src/CiTest_Client/AssemblyInfoClient.cs
+++ src/CiTest_Client/AssemblyInfoClient.cs
@@ -2,6 +2,6 @@
 using System.Reflection;
 using System.Resources;
 
-[assembly: AssemblyVersion( "5.0.1" )]
-[assembly: AssemblyInformationalVersion("5.0.1")]
-[assembly: AssemblyFileVersion( "5.0.1" )]
+[assembly: AssemblyVersion( "5.0.1.0" )]
+[assembly: AssemblyInformationalVersion("5.0.1.0")]
+[assembly: AssemblyFileVersion( "5.0.1.0" )]
